69c18d91d1514e48794ddbc2d1027c1e5b7a1ced
andyhills <andy.hills@flite.com>
2014-12-11 10:46:18 -0800

    v1.3

--- findAd.js
+++ findAd.js
@@ -11,6 +11,7 @@ chrome.webNavigation.onBeforeNavigate.addListener(
 			//console.log(details.frameId + " " + details.url);
 			guidList = [];
 			chrome.browserAction.setBadgeText({text: ""});
+			chrome.browserAction.setIcon({path: "icons/flite-finder-icons_inactive.png"}); 
 		}
 	}
 );
@@ -23,8 +24,10 @@ chrome.webRequest.onBeforeRequest.addListener(
 		theGuid = info.url.substring(guidLoc, guidLoc+36);
 		if (guidList.indexOf(theGuid) === -1  && theGuid.search('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}') === 0) {
 			guidList.push(theGuid);
+			chrome.browserAction.setIcon({path: "icons/flite-finder-icons_active.png"});
+			chrome.browserAction.setBadgeText({text: guidList.length.toString()});
 		}
-		chrome.browserAction.setBadgeText({text: guidList.length.toString()});
+		
 		//console.log(guidList);
 	},
 	//filters:
@@ -33,6 +36,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 	}
 );
 
+//open the flite ads in new tabs on click of the extension button
 chrome.browserAction.onClicked.addListener(
 	function(tab){
 		for (i = 0; i < guidList.length; i++){
